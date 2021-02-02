6c68e44b84bd6276d84ddaa0540a307445bfedf6
rick darlington <ricklusive@gmail.com>
2021-02-01 18:49:21 -0700

    embedded server with position working!

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using DarkRift;
+using DarkRift.Server;
+using DarkRift.Server.Unity;
+using UnityEngine;
+
+public class ServerManager : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField]
+    public GameObject playerPrefab;
+    
+    public static ServerManager Instance;
+
+    private XmlUnityServer xmlServer;
+    private DarkRiftServer server;
+    
+    public Dictionary<ushort, ServerPlayer> Players = new Dictionary<ushort, ServerPlayer>();
+    public Dictionary<string, ServerPlayer> PlayersByName = new Dictionary<string, ServerPlayer>();
+    
+    private List<NetworkingData.PlayerStateData> playerStateData = new List<NetworkingData.PlayerStateData>(4);
+    private List<NetworkingData.PlayerSpawnData> playerSpawnData = new List<NetworkingData.PlayerSpawnData>(4);
+    private List<NetworkingData.PlayerDespawnData> playerDespawnData = new List<NetworkingData.PlayerDespawnData>(4);
+    
+    public uint ServerTick = 0;
+    
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+    
+    void Start()
+    {
+        xmlServer = GetComponent<XmlUnityServer>();
+        server = xmlServer.Server;
+        server.ClientManager.ClientConnected += Connected;
+        server.ClientManager.ClientDisconnected += Disconnected;
+    }
+    
+    void OnDestroy()
+    {
+        server.ClientManager.ClientConnected -= Connected;
+        server.ClientManager.ClientDisconnected -= Disconnected;
+    }
+
+    void Connected(object sender, ClientConnectedEventArgs args)
+    {
+        Debug.Log($"{server.ClientManager.Count} client connections total.");
+        args.Client.MessageReceived += OnMessage;
+    }
+
+    void Disconnected(object sender, ClientDisconnectedEventArgs args)
+    {
+        IClient client = args.Client;
+        ServerPlayer player;
+        if (Players.TryGetValue(client.ID, out player))
+        {
+            Debug.Log($"{player.Name} disconnected.");
+            RemovePlayer(args.Client.ID, player.Name);
+        }
+
+        args.Client.MessageReceived -= OnMessage;
+    }
+
+    private void OnMessage(object sender, MessageReceivedEventArgs e)
+    {
+        IClient client = (IClient) sender;
+        using (Message message = e.GetMessage())
+        {
+            switch ((NetworkingData.Tags) message.Tag)
+            {
+                case NetworkingData.Tags.LoginRequest:
+                    OnClientLogin(client, message.Deserialize<NetworkingData.LoginRequestData>());
+                    break;
+                default:
+                    Debug.Log($"Unhandled tag: {message.Tag}");
+                    break;
+            }
+        }
+    }
+
+    private void OnClientLogin(IClient client, NetworkingData.LoginRequestData data)
+    {
+        Debug.Log($"Login request from ({data.Name}).");
+        if (PlayersByName.ContainsKey(data.Name))
+        {
+            using (Message message = Message.CreateEmpty((ushort) NetworkingData.Tags.LoginRequestDenied))
+            {
+                Debug.Log($"{data.Name} is already logged in!");
+                client.SendMessage(message, SendMode.Reliable);
+            }
+
+            return;
+        }
+
+        GameObject go = Instantiate(playerPrefab, transform);
+        ServerPlayer player = go.GetComponent<ServerPlayer>();
+        player.Initialize(client, ServerTick, data.Name, Instance);
+        client.MessageReceived -= OnMessage;
+
+        AddPlayer(client.ID, data.Name, player);
+    }
+
+    private void AddPlayer(ushort id, string name, ServerPlayer p)
+    {
+        Players.Add(id, p);
+        PlayersByName.Add(name, p);
+        Debug.Log($"{Players.Count} players online.");
+    }
+
+    public void RemovePlayer(ushort id, String name)
+    {
+        Players.Remove(id);
+        PlayersByName.Remove(name);
+        Debug.Log($"{Players.Count} players online.");
+    }
+    
+    
+    void FixedUpdate()
+    {
+        ServerTick++;
+
+        foreach (KeyValuePair<ushort, ServerPlayer> kv in Players)
+        {
+            ServerPlayer player = kv.Value;
+            playerStateData.Add(player.PlayerUpdate());
+        }
+
+        // Send update message to all players.
+        NetworkingData.PlayerStateData[] playerStateDataArray = playerStateData.ToArray();
+        NetworkingData.PlayerSpawnData[] playerSpawnDataArray = playerSpawnData.ToArray();
+        NetworkingData.PlayerDespawnData[] playerDespawnDataArray = playerDespawnData.ToArray();
+        
+        foreach (KeyValuePair<ushort, ServerPlayer> kv in Players)
+        {
+
+            using (Message m = Message.Create((ushort)NetworkingData.Tags.GameUpdate, new NetworkingData.GameUpdateData(kv.Value.InputTick, playerStateDataArray, playerSpawnDataArray, playerDespawnDataArray)))
+            {
+                kv.Value.Client.SendMessage(m, SendMode.Reliable);
+            }
+        }
+
+        playerStateData.Clear();
+        playerSpawnData.Clear();
+        playerDespawnData.Clear();
+    }
+}
---
+++ Assets/Scripts/ServerPlayer.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DarkRift;
+using DarkRift.Server;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+[RequireComponent(typeof(PlayerLogic))]
+public class ServerPlayer : MonoBehaviour
+{
+    private NetworkingData.PlayerStateData currentPlayerStateData;
+    private Buffer<NetworkingData.PlayerInputData> inputBuffer = new Buffer<NetworkingData.PlayerInputData>(1, 2);
+
+    private ServerManager Instance;
+    
+    public PlayerLogic PlayerLogic { get; private set; }
+    public uint InputTick { get; private set; }
+    public IClient Client { get; private set; }
+
+    public String Name = "";
+    
+    public List<NetworkingData.PlayerStateData> PlayerStateDataHistory { get; } = new List<NetworkingData.PlayerStateData>();
+
+    private NetworkingData.PlayerInputData[] inputs = {};
+
+    void Awake()
+    {
+        PlayerLogic = GetComponent<PlayerLogic>();
+    }
+
+    public void Initialize(IClient client, uint tick, String name, ServerManager instance)
+    {
+        Client = client;
+        currentPlayerStateData = new NetworkingData.PlayerStateData(client.ID, new Vector2(0, 0), 0);
+        InputTick = tick;
+        Name = name;
+        Instance = instance;
+        
+        Client.MessageReceived += OnMessage;
+        
+        transform.position = new Vector2(0,0);
+
+        Debug.Log($"Connection for {Name} configured, sending login accept.");
+
+        using (Message m = Message.Create((ushort) NetworkingData.Tags.LoginRequestAccepted,
+            new NetworkingData.LoginInfoData(client.ID)))
+        {
+            client.SendMessage(m, SendMode.Reliable);
+        }
+    }
+
+    private void OnMessage(object sender, MessageReceivedEventArgs args)
+    {
+        using (Message message = args.GetMessage())
+        {
+            switch ((NetworkingData.Tags) message.Tag)
+            {
+                case NetworkingData.Tags.PlayerReady:
+                    PlayerReady();
+                    break;
+                case NetworkingData.Tags.GamePlayerInput:
+                    RecieveInput(message.Deserialize<NetworkingData.PlayerInputData>());
+                    break;
+                default:
+                    Debug.Log($"Unhandled tag: {message.Tag}");
+                    break;
+            }
+        }
+    }
+
+    private void PlayerReady()
+    {
+        NetworkingData.PlayerSpawnData[] players = new NetworkingData.PlayerSpawnData[1];
+        players[0] = new NetworkingData.PlayerSpawnData(Client.ID, Name, transform.position);
+        //players[1] = new NetworkingData.PlayerSpawnData(99, "agent 99", Vector2.Zero);
+
+        using (Message m = Message.Create((ushort) NetworkingData.Tags.GameStartData,
+            new NetworkingData.GameStartData(players, Instance.ServerTick)))
+        {
+            Debug.Log("Sending Game Start Data");
+
+            Client.SendMessage(m, SendMode.Reliable);
+        }
+    }
+
+    public void RecieveInput(NetworkingData.PlayerInputData input)
+    {
+        inputBuffer.Add(input);
+    }
+
+    public NetworkingData.PlayerStateData PlayerUpdate()
+    {
+        inputs = inputBuffer.Get();
+        if (inputs.Length > 0)
+        {
+            NetworkingData.PlayerInputData input = inputs.First();
+            InputTick++;
+
+            for (int i = 1; i < inputs.Length; i++)
+            {
+                InputTick++;
+                for (int j = 0; j < input.Keyinputs.Length; j++)
+                {
+                    input.Keyinputs[j] = input.Keyinputs[j] || inputs[i].Keyinputs[j];
+                }
+                input.LookDirection = inputs[i].LookDirection;
+            }
+
+            currentPlayerStateData = PlayerLogic.GetNextFrameData(Client.ID, input);
+        }
+        
+        PlayerStateDataHistory.Add(currentPlayerStateData);
+        if (PlayerStateDataHistory.Count > 10)
+        {
+            PlayerStateDataHistory.RemoveAt(0);
+        }
+
+        transform.localPosition = currentPlayerStateData.Position;
+        return currentPlayerStateData;
+    }
+}
